3f57dc9d814d8f03b6e34b10222270571d9c6876
Dani BL <danibachsl@protonmail.com>
2022-06-10 10:19:31 +0200

    arreglos

--- src/index.js
+++ src/index.js
@@ -1,62 +1,65 @@
-const canvas = document.getElementById("game");
+const canvas = document.querySelector("#canvas");
 const ctx = canvas.getContext("2d");
 
-canvas.style.background = "lightgray";
+canvas.style.backgroundColor = "lightgray";
 
-let pelotaImage = new Image();
-pelotaImage.src = "src/Pelota.png";
+let cocheImagen = new Image();
+cocheImagen.src = "src/destructor.png";
 
-let destructorImagen = new Image();
-destructorImagen.src = "/Users/danibachslobo/Desktop/IRONHACK/GAME PROJECT/src/images/destructor.png";
-
-let submarinoImagen = new Imagen();
-submarinoImagen.src = "/Users/danibachslobo/Desktop/IRONHACK/GAME PROJECT/src/images/icono_submarino.png";
+let obstaculoImagen = new Image();
+obstaculoImagen.src = "src/icono_submarino.png";
 
 const obstaculos = [];
 
-const destructor = new Objeto(250, 0, 60, destructorImagen, ctx);
+const coche = new Objeto(731.5, 75, 163, 37, cocheImagen, ctx);
 
 const jugar = () => {
-    for (let obstaculo of obstaculos) {
-        obstaculo.borrar();
-        obstaculo.y -= 10;
-        obstaculo.dibujar();
-        if (destructor.detectarColision(obstaculo)) {
-            console.log("¡Le has dado!");
-        }
+  for (let obstaculo of obstaculos) {
+    obstaculo.borrar();
+    obstaculo.y -= 5;
+    obstaculo.dibujar();
+    if (coche.detectarColision(obstaculo)) {
+      console.log("Has perdido");
     }
+  }
 };
 
 const crearObstaculos = () => {
-    const randomPosicionY = Math.floor(Math.random() * 480);
-    const obstaculo = new Objeto(
-        randomPosicionY,
-        570,
-        120,
-        60,
-        obstaculoImagen,
-        ctx
-    );
-    obstaculos.push(obstaculo);
-}
-
-const cargaInicial = () => {
-    destructor.dibujar();
-    setInterval(jugar, 1000);
-    setInterval(crearObstaculos, 7000);
+  const randomPositionX = Math.floor(Math.random() * 480);
+  const obstaculo = new Objeto(
+    randomPositionX,
+    570,
+    120,
+    60,
+    obstaculoImagen,
+    ctx
+  );
+  obstaculos.push(obstaculo);
 };
 
-const moverDestructor = (e) => {
-    destructor.borrar();
-    if (e.key === "ArrowLeft") {
-        destructor.x -= 15;
-    }
-    if (e.key === "ArrowRight") {
-        destructor.x += 15;
-    }
-    destructor.dibujar();
+const cargaInicial = () => {
+  coche.dibujar();
+  setInterval(jugar, 200);
+  setInterval(crearObstaculos, 3000);
+};
+
+const moverCoche = (e) => {
+  coche.borrar();
+  if (e.key === "ArrowLeft") {
+    coche.x -= 5;
+  }
+  if (e.key === "ArrowRight") {
+    coche.x += 5;
+  }
+  if (e.key === "ArrowUp") {
+    coche.y -= 5;
+  }
+  if (e.key === "ArrowDown") {
+    coche.y += 5;
+  }
+  coche.dibujar();
 };
 
 window.addEventListener("load", cargaInicial);
 
-window.addEventListener("keydown", moverDestructor);
+window.addEventListener("keydown", moverCoche);
